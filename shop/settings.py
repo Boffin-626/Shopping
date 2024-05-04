d2fff0efa55745a8ccbfd02bf9cf3dc293bb3d06
Boffin <boffin626@icloud.com>
2024-05-04 23:12:33 +0200

    structuring the store.html template

--- shop/settings.py
+++ shop/settings.py
@@ -111,6 +111,14 @@ STATIC_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+MEDIA_URL = 'images/'
+
+MEDIA_ROOT = [
+    os.path.join(BASE_DIR, 'images')
+]
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
