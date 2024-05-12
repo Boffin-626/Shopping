367e63bcbc4a45e4354d5f4f9310ff536ccff32d
Boffin <boffin626@icloud.com>
2024-05-12 20:37:40 +0200

    Cart not updating => debugging!

--- shopping/static/javascript/cart.js
+++ shopping/static/javascript/cart.js
@@ -1,8 +1,8 @@
 var updateBtns = document.getElementsByClassName('update-cart')
 
-for(var i = 0; i < update-updateBtns.length; i++){
-    updateBtn[i].addEventListener('click', function(){
-        var productId = this.dataste.product
+for(var i = 0; i < updateBtns.length; i++){
+    updateBtns[i].addEventListener('click', function(){
+        var productId = this.dataset.product
         var action = this.dataset.action
 
         console.log ('productId:', productId, 'Action:', action)
